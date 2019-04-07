f9259e9f9b9bd18f1fa3eef310b8bd72fd8fb1fc
koy <kosatun@aol.com>
2019-04-07 00:37:54 -0700

    deleted some files. Modified App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayBio: false };
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+  }
+  
+  toggleDisplayBio() {
+    this.setState({ displayBio: !this.state.displayBio });
+  }
+  
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+     return (
+      <div>
+        <h1>Hello. Welcome To My Page</h1>
+        <p>My name is Koy. I'm a software engineer.</p>
+        <p>I'm always looking forward to working on meaningful projects</p>
+         {this.state.displayBio ? (<div>
+           <p>I live in the Bay Area of California. I love to learn and I code everyday.</p>
+           <p>Although my favorite language is Javascript, I am open to building
+             projects with other languages and frameworks.</p>
+           <p>Besides coding, I love to read and listen to music.</p>
+           <button onClick={ this.toggleDisplayBio }>Show Less</button>
+         </div>) : (
+           <div>
+             <button onClick={ this.toggleDisplayBio }>Read More</button>
+           </div>
+         ) }
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App; 
